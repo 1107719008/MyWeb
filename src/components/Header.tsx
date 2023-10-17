dc6eda42040776a55515e9ad6c844dba4dc4507c
Chun-Han-Lin <hanklin0208@gmail.com>
2023-10-17 17:23:09 +0800

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <div>
+     <h1>Hi, I'm Hank Lin. A Developer.</h1>
+      <p>這是header。</p>
+      
+     
+    </div>
+  );
+};
+
+export default Header;
+
---
+++ src/components/TestetComponent.tsx
@@ -0,0 +1,14 @@
+// TestetComponent.tsx
+import React from 'react';
+
+const TestetComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>歡迎使用React和Vite</h1>
+      <p>這是一個簡單的範例，渲染文字和圖片。</p>
+      <img src="/path/to/your/image.jpg" alt="示例圖片" />
+    </div>
+  );
+};
+
+export default TestetComponent;
